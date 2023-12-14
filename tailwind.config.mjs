18f90b872057f3830303b555cc471373d4adb822
Rifky Alamsyah <rifkyalamsyah@Rifkys-MacBook-Pro.local>
2023-12-15 04:02:58 +0700

    initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
