7ff432f834b5d11b26f3845c038daf4e2c1afa32
Rifky Alamsyah <rifkyalamsyah@Rifkys-MacBook-Pro.local>
2024-03-08 19:43:35 +0700

    add sitemap

--- src/pages/rrs.xml.ts
+++ src/pages/rrs.xml.ts
@@ -1,7 +1,7 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
-export async function GET(context) {
+export async function GET(context: { site: any; }) {
   const blog = await getCollection('blog');
   return rss({
     title: 'Rifky Alamsyah | Blog',
