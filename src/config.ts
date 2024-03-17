4686384f3b4d51da2a06ba1d9e71504b10840ec1
Rifky <rifky@Rifkys-MacBook-Pro.local>
2024-03-17 21:14:12 +0700

    Update SEO

--- src/config.ts
+++ src/config.ts
@@ -1,35 +1,35 @@
 export const HOME_PAGE_SEO = {
   title: 'Rifky Alamsyah - UI Designer & Frontend Dev',
-  seoDesc: 'I love solving problems and implementing ideas by design.',
+  seoDesc: 'Explore the innovative designs and flawless front-end web development skills of Rifky Alamsyah, a seasoned UI designer. Discover his portfolio showcasing creativity and expertise in user interface design and front-end development.',
   ogImage: 'https://rifkyalamsyah.github.io/images/og/og-rifky.png',
   url: 'https://rifkyalamsyah.github.io',
   domain: 'rifkyalamsyah.github.io',
   keywords:
-    'rifky, rifky alamsyah, ui designer , ui/ux designer, web designer, frontend developer, webflow developer, freelancer, rifky alamsyah freelance designer, rifky alamsyah ui designer, rifky alamsyah ui/ux designer, rifky alamsyah web designer, rifky alamsyah webflow developer, rifky alamsyah frontend developer',
+    'ui designer, front-end web development, ui/ux designer, web designer, user interface design, user experience design, website development, front-end developer creative web design, responsive web design, ui/ux design, html/css, javascript, portfolio showcase. web design project, digital design expertise, webflow, webflow developer, webflow designer, rifky, rifky alamsyah, rifky dev, rifky ui designer, rifky webflow developer, rifky portfolio, rifky project, rifky blog'
 };
 
 export const ABOUT_PAGE_SEO = {
   title: 'About - Rifky Alamsyah',
   seoDesc:
-    'UI Designer & Front-End Developer, I love solving problems and implementing ideas by design.',
+    'Learn more about Rifky Alamsyah, his resume, projects, digital products, and insightful blog. Explore his professional journey, innovative projects, and valuable blog posts on design, development, and digital products.',
   ogImage: 'https://rifkyalamsyah.github.io/images/og/og-rifky.png',
   url: 'https://rifkyalamsyah.github.io/about',
   domain: 'rifkyalamsyah.github.io/about',
   keywords:
-    'rifky, rifky alamsyah, ui designer , ui/ux designer, web designer, frontend developer, webflow developer, freelancer, rifky alamsyah freelance designer, rifky alamsyah ui designer, rifky alamsyah ui/ux designer, rifky alamsyah web designer, rifky alamsyah webflow developer, rifky alamsyah frontend developer',
+    'ui designer, front-end web development, ui/ux designer, web designer, user interface design, user experience design, website development, front-end developer creative web design, responsive web design, ui/ux design, html/css, javascript, portfolio showcase. web design project, digital design expertise, webflow, webflow developer, webflow designer, rifky, rifky alamsyah, rifky dev, rifky ui designer, rifky webflow developer, rifky portfolio, rifky project, rifky blog',
 };
 
 export const PROJECT_PAGE_SEO = {
-  title: 'Projects - Rifky Alamsyah',
-  seoDesc: 'Discover my portfolio',
+  title: 'Projects: Innovative Designs & Development | Rifky Alamsyah',
+  seoDesc: 'Discover Rifky Alamsyah impressive portfolio of projects, showcasing his expertise in innovative designs and development. Explore a range of captivating projects demonstrating creativity and technical proficiency.',
   ogImage: 'https://rifkyalamsyah.github.io/images/og/og-project.png',
   url: 'https://rifkyalamsyah.github.io/project',
   domain: 'rifkyalamsyah.github.io/project',
 };
 
 export const BLOG_PAGE_SEO = {
-  title: 'Blog - Rifky Alamsyah',
-  seoDesc: 'Explore my insightful articles',
+  title: 'Insightful Blog: Design, Development & Digital Trends | Rifky Alamsyah',
+  seoDesc: 'Explore Rifky Alamsyah engaging blog, offering valuable insights on design, development, and digital trends. Stay updated with informative articles, tutorials, and thought-provoking discussions in the ever-evolving world of technology.',
   ogImage: 'https://rifkyalamsyah.github.io/images/og/og-blog.png',
   url: 'https://rifkyalamsyah.github.io/blog',
   domain: 'rifkyalamsyah.github.io/blog',
