60ac951bbd34f54ca13f29c76ccd41aeef113cec
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2024-04-03 02:57:28 +0700

    add back to top

---
+++ src/data/seo.ts
@@ -0,0 +1,32 @@
+export const HOME_PAGE_SEO = {
+    title: 'Rifky Alamsyah - UI Designer & Frontend Dev',
+    seoDesc: 'Discover his portfolio showcasing creativity and expertise in user interface design and front-end development.',
+    ogImage: 'https://rifkyalamsyah.github.io/images/og/og-rifky.png',
+    url: 'https://rifkyalamsyah.github.io',
+    domain: 'rifkyalamsyah.github.io',
+  };
+  
+  export const ABOUT_PAGE_SEO = {
+    title: 'About - Rifky Alamsyah',
+    seoDesc:
+      'Learn more about Rifky Alamsyah, Explore his professional journey, innovative projects, and valuable blog posts on design, development, and digital products.',
+    ogImage: 'https://rifkyalamsyah.github.io/images/og/og-rifky.png',
+    url: 'https://rifkyalamsyah.github.io/about',
+    domain: 'rifkyalamsyah.github.io/about',
+  };
+  
+  export const PROJECT_PAGE_SEO = {
+    title: 'Projects - Rifky Alamsyah',
+    seoDesc: 'Discover Rifky Alamsyah impressive portfolio of projects, showcasing his expertise in innovative designs and development.',
+    ogImage: 'https://rifkyalamsyah.github.io/images/og/og-project.png',
+    url: 'https://rifkyalamsyah.github.io/project',
+    domain: 'rifkyalamsyah.github.io/project',
+  };
+  
+  export const BLOG_PAGE_SEO = {
+    title: 'Blog - Rifky Alamsyah',
+    seoDesc: 'Explore Rifky Alamsyah blog, offering valuable insights on design, development, and digital trends.',
+    ogImage: 'https://rifkyalamsyah.github.io/images/og/og-blog.png',
+    url: 'https://rifkyalamsyah.github.io/blog',
+    domain: 'rifkyalamsyah.github.io/blog',
+  };
