e0905aac3bf9cf0e4548b7747e1886a3d8ce49af
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2024-04-20 10:47:32 +0700

    upgrade astro & update contents

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,17 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
-import playformCompress from "@playform/compress";
+// import playformCompress from "@playform/compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap(), partytown(), playformCompress()],
+  integrations: [tailwind(), mdx(), sitemap(), partytown(), (await import("@playform/compress")).default({
+    Image: false,
+    SVG: false,
+  })],
   output: 'static',
   prefetch: true,
   compressHTML: true,
   site: 'https://rifkyalamsyah.github.io',
-  base: '/'
+  base: '/',
 });
